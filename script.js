a96714d2bc9144d8e3b640d82d2d6639afa82913
dmozzoni <cyric@hotmail.com>
2017-01-02 22:29:04 -0500

    initial

---
+++ script.js
@@ -0,0 +1,8 @@
+$( "#button4" ).on( "click", function() {
+  window.location.href = 'files/Mozzoni_resume.pdf';
+  return false;
+});
+$( "#button1" ).on( "click", function() {
+  window.location.href = 'https://dmozzoni.github.io/ProjectOne/';
+  return false;
+});
