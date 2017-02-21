6774b7f732a49c76be5fb4a1eb244d1c8da165af
dmozzoni <cyric@hotmail.com>
2017-02-20 20:22:08 -0500

    add project 4 and new cv

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-$( "#button4" ).on( "click", function() {
-  window.location.href = 'files/Mozzoni_resume.pdf';
-  return false;
-});
-$( "#button1" ).on( "click", function() {
-  window.location.href = 'https://dmozzoni.github.io/ProjectOne/';
-  return false;
-});
-$( "#button2" ).on( "click", function() {
-  window.location.href = 'http://butterfly-life.herokuapp.com/';
-  return false;
+$(document).ready(function() {
+    $("#button1").on("click", function() {
+        window.location.href = 'https://dmozzoni.github.io/ProjectOne/';
+        return false;
+    });
+    $("#button2").on("click", function() {
+        window.location.href = 'http://butterfly-life.herokuapp.com/';
+        return false;
+    });
+    $("#button4").on("click", function() {
+        window.location.href = 'files/Mozzoni_resume.pdf';
+        return false;
+    });
+    $("#button5").on("click", function() {
+        window.location.href = 'https://nonogrid.herokuapp.com/ ';
+        return false;
+    });
 });
