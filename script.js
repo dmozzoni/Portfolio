232949c6c98a3166d38015c6702d989168fb9fea
dmozzoni <cyric@hotmail.com>
2017-02-28 16:39:54 -0500

    update buttons to include github

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 $(document).ready(function() {
-    $("#button1").on("click", function() {
-        window.location.href = 'https://dmozzoni.github.io/ProjectOne/';
-        return false;
-    });
-    $("#button2").on("click", function() {
-        window.location.href = 'http://butterfly-life.herokuapp.com/';
-        return false;
-    });
+
     $("#button4").on("click", function() {
         window.location.href = 'files/Mozzoni_resume.pdf';
         return false;
     });
-    $("#button5").on("click", function() {
-        window.location.href = 'https://nonogrid.herokuapp.com/ ';
+
+    $("#button6").on("click", function() {
+        window.location.href = 'https://profiles.generalassemb.ly/profiles/david-mozzoni';
         return false;
     });
+
+    $('#button1').hover(function() {
+        $('#b11, #b12, #b13').toggleClass('hide');
+    });
+
+    $('#button2').hover(function() {
+        $('#b21, #b22, #b23').toggleClass('hide');
+    });
+
+    $('#button5').hover(function() {
+        $('#b51, #b52, #b53').toggleClass('hide');
+    });
+
 });
